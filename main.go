8cd2cdd42e7014a3d9797acdcdf4e86d36b860b1
erfan <mahdi.farahmandsafa@gmail.com>
2025-02-27 10:43:51 +0330

    add CORS policies to app

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/erfanfs10/Lab-Backend/middlewares"
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
@@ -11,6 +12,16 @@ func main() {
 
 	e.Use(middlewares.CustomLogger())
 	e.Use(middlewares.SeparateLogs())
+
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"*"},
+		AllowMethods: []string{echo.GET, echo.POST,
+			echo.PUT, echo.PATCH, echo.DELETE},
+		AllowHeaders: []string{echo.HeaderOrigin,
+			echo.HeaderContentType, echo.HeaderAccept,
+			echo.HeaderAuthorization},
+		AllowCredentials: true,
+	}))
 
 	e.Logger.Fatal(e.Start(":8000"))
 }
